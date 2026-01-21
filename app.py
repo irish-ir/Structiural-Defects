8862b11e1cbc93e439728bd1b088705c73e7bfc4
irish-ir <irishranaa@gmail.com>
2026-01-21 10:44:19 +0530

    v1

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st 
-import google.generativeai as genai
 import datetime as dt
 import os 
 from dotenv import load_dotenv
 load_dotenv()
+import google.generativeai as genai
 import datetime as dt
 from PIL import Image
 
